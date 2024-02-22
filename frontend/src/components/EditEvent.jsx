a838756b10d45f3d7061dd0d2e319af77d94f933
MaximeGillot5 <mgillot56@icloud.com>
2024-02-22 15:18:41 +0100

    edit event

---
+++ frontend/src/components/EditEvent.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const EditEvent = ({ eventId, onUpdate }) => {
+  // State pour stocker les données du formulaire
+  const [formData, setFormData] = useState({
+    event_name: "",
+    description: "",
+    location: "",
+  });
+
+  // Effet pour mettre à jour les valeurs initiales du formulaire lorsque l'événement change
+  useEffect(() => {
+    // Récupérer les détails de l'événement depuis l'API
+    const fetchEventDetails = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/v1/events/${eventId}`
+        );
+        // Mettre à jour l'état local avec les détails de l'événement
+        setFormData(response.data);
+      } catch (error) {
+        console.error(
+          "Erreur lors de la récupération des détails de l'événement:",
+          error
+        );
+      }
+    };
+
+    // Appeler la fonction pour récupérer les détails de l'événement
+    fetchEventDetails();
+  }, [eventId]); // Effect ne s'exécute que lorsque l'ID de l'événement change
+
+  // Gestionnaire de changement pour mettre à jour le state lorsque les champs sont modifiés
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Gestionnaire de soumission pour envoyer les données du formulaire à l'API Ruby
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Envoi des données du formulaire à l'API Ruby pour mettre à jour l'événement
+      const response = await axios.put(
+        `http://localhost:3000/api/v1/events/${eventId}`,
+        formData
+      );
+      console.log("Réponse de l'API Ruby:", response.data);
+      // Appeler la fonction onUpdate pour mettre à jour la liste des événements dans le composant parent
+      onUpdate();
+    } catch (error) {
+      console.error("Erreur lors de la mise à jour de l'événement:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Nom de l'événement:</label>
+        <input
+          type="text"
+          name="event_name"
+          value={formData.event_name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Description:</label>
+        <textarea
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          required
+        ></textarea>
+      </div>
+      <div>
+        <label>Location:</label>
+        <input
+          type="text"
+          name="location"
+          value={formData.location}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Modifier l'événement</button>
+    </form>
+  );
+};
+
+export default EditEvent;
--- frontend/src/components/EventsList.jsx
+++ frontend/src/components/EventsList.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import DeleteEvent from "./DeleteEvent";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import EditEvent from "./EditEvent";
 
 const EventsList = (props) => {
-  const handleDelete = async (eventId) => {
+  const handleUpdate = async () => {
     try {
-      // Supprimer l'événement de la liste des événements dans le composant parent
-      const updatedEvents = props.events.filter(
-        (event) => event.id !== eventId
-      );
-      // Mettre à jour la liste des événements dans le composant parent avec la nouvelle liste filtrée
-      props.setEvents(updatedEvents);
+      // Mettre à jour la liste des événements en récupérant les données actualisées de l'API
+      const response = await axios.get("http://localhost:3000/api/v1/events");
+      props.setEvents(response.data);
     } catch (error) {
-      console.error("Erreur lors de la suppression de l'événement:", error);
+      console.error(
+        "Erreur lors de la mise à jour de la liste des événements:",
+        error
+      );
     }
   };
+
   return (
     <div>
       <h1>Events de l'API</h1>
@@ -26,7 +29,14 @@ const EventsList = (props) => {
             <p>{event.location}</p>
             <DeleteEvent
               eventId={event.id}
-              onDelete={() => handleDelete(event.id)}
+              onDelete={() => handleUpdate(event.id)}
+            />
+            <Link to={`/edit-event/${event.id}`}>
+              <button>Editer</button>
+            </Link>
+            <EditEvent
+              eventId={event.id}
+              onUpdate={() => handleUpdate(event.id)} // Utilisez handleUpdate pour mettre à jour la liste des événements après édition
             />
           </div>
         );
